7fcfdbcf9d36d711dfd02e98b8445eae2364fa46
WarV7 <TheWar@FBI.al>
2014-01-30 21:21:16 -0500

    Hiding HUD cl_hooks

--- gamemode/cl_hooks.lua
+++ gamemode/cl_hooks.lua
@@ -1,5 +1,22 @@
 -- hi this isnt included yet. bye
 
+local DontDraw = {
+	"CHudHealth",
+	"CHudSuitPower",
+	"CHudBattery",
+	"CHudCrosshair",
+	"CHudAmmo",
+	"CHudSecondaryAmmo",
+	"CHudChat"
+
+}
+
 function GM:HUDPaint()
 	
+end
+
+function GM:HUDShouldDraw(dv)
+	if (dv == table.HasValue(DontDraw, dv)) then
+		return false
+	end
 end
